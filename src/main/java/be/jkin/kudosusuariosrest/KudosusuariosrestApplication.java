b414c3c9582c0af9e60db7153dc1b34e9e1c0003
pablovillazon <p.villazon@gmail.com>
2019-09-07 13:30:21 -0400

    Initial Maven code

---
+++ src/main/java/be/jkin/kudosusuariosrest/KudosusuariosrestApplication.java
@@ -0,0 +1,13 @@
+package be.jkin.kudosusuariosrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudosusuariosrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudosusuariosrestApplication.class, args);
+	}
+
+}
