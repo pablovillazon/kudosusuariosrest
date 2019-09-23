19b0dcae5fa6ca97290f04da211bbea32277b6dc
pablovillazon <p.villazon@gmail.com>
2019-09-22 21:44:40 -0400

    Changes to user controller

--- src/main/java/be/jkin/kudosusuariosrest/Controllers/UserController.java
+++ src/main/java/be/jkin/kudosusuariosrest/Controllers/UserController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
